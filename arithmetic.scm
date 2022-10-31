86681070a9f79749a2febab12e6cf93d1cadcb19
jyotiskaghosh <31136853+jyotiskaghosh@users.noreply.github.com>
2022-10-31 21:57:09 +0530

    Update arithmetic.scm

--- arithmetic.scm
+++ arithmetic.scm
@@ -3,7 +3,7 @@
 (define (addition m)
 	(define (add x y)
 		(if (or (= x 0) (= y 0) (not (integer? x)) (not (integer? y))) 
-			(error "Invalid output")
+			(error "Invalid argument")
 			(mod (+ x y) m)))
 	(if (<= m 0) 
 		(error "Negative numbers and 0 are invalid modulo value")
