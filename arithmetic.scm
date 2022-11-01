f8a991c3208280e910437331512286937f4fa8fa
Jyotiska <jyotiskaghosh@gmail.com>
2022-11-01 09:30:15 +0530

    added some checks

--- arithmetic.scm
+++ arithmetic.scm
@@ -1,11 +1,13 @@
 (define (mod x y) (let ((m (modulo x y))) (if (= m 0) y m)))
 
+(define (check? x) (and (integer? x) (> x 0)))
+
 (define (addition m)
 	(define (add x y)
-		(if (or (= x 0) (= y 0) (not (integer? x)) (not (integer? y))) 
-			(error "Invalid output")
-			(mod (+ x y) m)))
-	(if (<= m 0) 
-		(error "Negative numbers and 0 are invalid modulo value")
-		(lambda (x y . numbers) (fold-right add (add x y) numbers))))
+		(if (and (check? x) (check? y) (<= x m) (<= y m))
+			(mod (+ x y) m) 
+			(error "Invalid arguments")))
+	(if (check? m) 
+		(lambda (x y . numbers) (fold-right add (add x y) numbers))
+		(error "Negative numbers and 0 are invalid modulo value")))
 
